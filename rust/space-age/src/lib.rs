e2bf452fe628ede9572add176eb6d070eb1c2222
Chen Li <chencassc@gmail.com>
2020-12-20 21:29:39 +0800

    use macros in rust/space-age

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -8,58 +8,31 @@ impl From<u64> for Duration {
 }
 
 pub trait Planet {
-    fn ob() -> f64;
+    const SCALE: f64;
     fn years_during(d: &Duration) -> f64 {
-        d.0 as f64 / Self::ob() / 31557600.0
+        d.0 as f64 / Self::SCALE / 31557600.0
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! planet_scales {
+    ($($name:ident -> $scale:literal),+) => {
+        $(
+            pub struct $name;
 
-impl Planet for Mercury {
-    fn ob() -> f64 {
-        0.2408467
+            impl Planet for $name {
+                const SCALE: f64 = $scale;
+            }
+        )+
     }
 }
-impl Planet for Venus {
-    fn ob() -> f64 {
-        0.61519726
-    }
-}
-impl Planet for Earth {
-    fn ob() -> f64 {
-        1.0
-    }
-}
-impl Planet for Mars {
-    fn ob() -> f64 {
-        1.8808158
-    }
-}
-impl Planet for Jupiter {
-    fn ob() -> f64 {
-        11.862615
-    }
-}
-impl Planet for Saturn {
-    fn ob() -> f64 {
-        29.447498
-    }
-}
-impl Planet for Uranus {
-    fn ob() -> f64 {
-        84.016846
-    }
-}
-impl Planet for Neptune {
-    fn ob() -> f64 {
-        164.79132
-    }
+
+planet_scales!{
+    Mercury -> 0.2408467,
+    Venus -> 0.61519726,
+    Earth -> 1.0,
+    Mars -> 1.8808158,
+    Jupiter -> 11.862615,
+    Saturn -> 29.447498,
+    Uranus -> 84.016846,
+    Neptune -> 164.79132
 }
