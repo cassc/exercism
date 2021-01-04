e1a035c55c9982788a3a2d24fae5acdf08391d35
Chen Li <chencassc@gmail.com>
2021-01-04 13:11:07 +0800

    use formulae in difference_of_squares.c

--- c/difference-of-squares/src/difference_of_squares.c
+++ c/difference-of-squares/src/difference_of_squares.c
@@ -1,19 +1,13 @@
 #include "difference_of_squares.h"
 #include <math.h>
 
-unsigned int sum_of_squares(unsigned int number){
-  unsigned int r = 0;
-  for (unsigned int i = 1; i <=number; i++){
-    r += pow(i, 2);
-  }
+unsigned int sum_of_squares(unsigned int n){
+  unsigned int r = n * (n + 1) * (2 * n + 1) / 6;
   return r;
 }
-unsigned int square_of_sum(unsigned int number) {
-  unsigned int r = 0;
-  for (unsigned int i = 1; i <= number; i++){
-    r += i;
-  }
-  return pow(r, 2);
+unsigned int square_of_sum(unsigned int n) {
+  unsigned int r = n * (n + 1) / 2;
+  return r * r;
 }
 
 unsigned int difference_of_squares(unsigned int number){
